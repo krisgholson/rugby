23fe165b14813759ace19a6a232ea33ebace4a51
Kris Gholson <kris@idea2.dev>
2021-11-29 22:34:56 -0500

    filter the list to only WRFC

--- sportlomo/members.py
+++ sportlomo/members.py
@@ -3,7 +3,7 @@ import pandas as pd
 import sportlomo
 
 EXPORT_COLUMNS = ['club', 'first_name', 'last_name', 'gender', 'membership_package']
-SORT_BY = 'last_name'
+SORT_BY = ['club', 'membership_package', 'gender', 'last_name', 'first_name']
 
 
 def main():
@@ -13,9 +13,13 @@ def main():
     data_frame = pd.DataFrame.from_records(data=members, index='national_number')
 
     # print(members)
-    print(len(members))
-    data_frame.sort_values(by=SORT_BY, inplace=True)
-    data_frame.to_csv('./dist/sportlomo_sc_export.csv', columns=EXPORT_COLUMNS)
+    print('total members length ' + str(len(members)))
+    data_frame.sort_values(by=SORT_BY, inplace=True, key=lambda col: col.str.lower())
+    filtered_df = data_frame.query("club == 'Charleston Youth and Junior Rugby' "
+                                   "or club == 'Wando Rugby Football Club'")
+    print('filtered members length (WRFC) ' + str(len(filtered_df.index)))
+
+    filtered_df.to_csv('./dist/sportlomo_sc_export.csv', columns=EXPORT_COLUMNS)
 
 
 if __name__ == "__main__":
